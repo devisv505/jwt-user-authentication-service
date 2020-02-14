9ce83eb0860f0e46acb8c5fcf07a784a9053eef1
devisv505 <devisv505@gmail.com>
2020-02-14 16:01:38 +0300

    init

---
+++ src/main/java/com/devisv/jwt/user/authentication/service/JwtUserAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devisv.jwt.user.authentication.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtUserAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devisv/jwt/user/authentication/service/JwtUserAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devisv.jwt.user.authentication.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtUserAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
